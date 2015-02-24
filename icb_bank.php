380c14d2ce7775d18ea75fe2fe26101fda80afb2
JimSatoshi <satoshi@internetcentralbank.org>
2015-02-23 20:40:53 -0800

    Update icb_bank.php

--- icb_bank.php
+++ icb_bank.php
@@ -7,7 +7,7 @@ Version: 0.1
 Author: Jim Maguire
 Author URI: http://customrayguns.com
 */
-[google](www.google.com)
+//[google](www.google.com)
 //The bootloader launches the varies features of the plugin:
 $icb_bank = new icb_bank_bootloader;
 
